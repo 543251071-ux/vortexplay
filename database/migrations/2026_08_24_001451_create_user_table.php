d0c681b2fd948ce9e397a24457f510442b5c2456
Rayyan <543251071@smktelkom-bjb.sch.id>
2026-08-24 08:25:05 +0800

    test

---
+++ database/migrations/2026_08_24_001451_create_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id(id_user);
+            $table->string('username', 255)->unique();
+            $table->string('nickname', 100);
+            $table->string('profil', 100 );
+            $table->string('deskripsi')>nullable();
+            $table->string('email');
+            $table->string('level', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
